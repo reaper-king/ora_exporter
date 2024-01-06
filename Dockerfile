e6fcd69b92cfc8e2de275033f71ce64bad6b99ad
Marcques Mouton <marcquesmouton1992@gmail.com>
2024-01-06 23:55:34 +0200

    add instant client for oracle

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,15 @@ RUN npm ci
 COPY . .
 RUN npm run build
 RUN npm prune --production
+RUN wget https://download.oracle.com/otn_software/linux/instantclient/2112000/instantclient-basic-linux.x64-21.12.0.0.0dbru.zip -O client.zip
+RUN unzip client.zip
 
 FROM node:18-alpine
 WORKDIR /app
 COPY --from=builder /app/build build/
 COPY --from=builder /app/node_modules node_modules/
 COPY package.json .
+COPY --from=builder /app/instantclient_21_12 client
 EXPOSE 3000
 ENV NODE_ENV=production
 CMD [ "node", "build" ]
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,13 +1,13 @@
 import oracledb from 'oracledb';
 import 'dotenv/config';
 
-const { CONNECTION_STRING , DB_PASS ,DB_USER  , INSTANT_CLIENT } = process.env
+const { CONNECTION_STRING , DB_PASS ,DB_USER  } = process.env
 
 let result
 
 
 try {
-    oracledb.initOracleClient({libDir: INSTANT_CLIENT});
+    oracledb.initOracleClient({libDir: '/app/client'});
   } catch (err) {
     console.error('Whoops!');
     console.error(err);
@@ -15,7 +15,6 @@ try {
   }
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
 
 
 export default async function sendOracleResult(queri:string) {
